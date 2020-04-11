bd9abf7075e8efa309d3ae82c55877f33abec59f
Abdullah DURSUN <adursun@outlook.com.tr>
2020-04-11 19:05:05 +0300

    Implement ItTaskService and BusinessTaskService

---
+++ src/Service/BusinessTaskService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Serializer\BusinessTaskSerializer;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class BusinessTaskService implements TaskServiceInterface
+{
+    private const BASE_URL = 'http://www.mocky.io/v2/5d47f235330000623fa3ebf7';
+
+    private HttpClientInterface $client;
+
+    private BusinessTaskSerializer $serializer;
+
+    public function __construct(HttpClientInterface $client, BusinessTaskSerializer $serializer)
+    {
+        $this->client = $client;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @return \App\Model\BusinessTask[]
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function getTasks()
+    {
+        $response = $this->client->request('GET', self::BASE_URL);
+        return $this->serializer->deserializeMany($response->getContent());
+    }
+}
---
+++ src/Service/ItTaskService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Serializer\ItTaskSerializer;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ItTaskService implements TaskServiceInterface
+{
+    private const BASE_URL = 'http://www.mocky.io/v2/5d47f24c330000623fa3ebfa';
+
+    private HttpClientInterface $client;
+
+    private ItTaskSerializer $itTaskSerializer;
+
+    public function __construct(HttpClientInterface $client, ItTaskSerializer $serializer)
+    {
+        $this->client = $client;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @return \App\Model\ItTask[]
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function getTasks()
+    {
+        $response = $this->client->request('GET', self::BASE_URL);
+        return $this->serializer->deserializeMany($response->getContent());
+    }
+}
