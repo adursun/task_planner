b696eb23821b9dea33e6fe98270724497c7a4771
Abdullah DURSUN <adursun@outlook.com.tr>
2020-04-12 14:58:49 +0300

    Add test cases with valid task lists

--- tests/Service/BusinessTaskServiceTest.php
+++ tests/Service/BusinessTaskServiceTest.php
@@ -10,12 +10,37 @@ use PHPUnit\Framework\TestCase;
 
 class BusinessTaskServiceTest extends TestCase
 {
+    private $mockClient;
+
+    private $service;
+
+    protected function setUp()
+    {
+        $this->mockClient = $this->createMock(Client::class);
+        $this->service = new BusinessTaskService($this->mockClient, new BusinessTaskSerializer());
+    }
+
     public function testGetTasksWithEmptyList(): void
     {
-        $mockClient = $this->createMock(Client::class);
-        $mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body='[]'));
-        $service = new BusinessTaskService($mockClient, new BusinessTaskSerializer());
+        $this->mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body='[]'));
+        $tasks = $this->service->getTasks();
 
-        $this->assertEquals([], $service->getTasks());
+        $this->assertCount(0, $tasks);
+        $this->assertEquals([], $tasks);
+    }
+
+    public function testGetTasksWithValidList(): void
+    {
+        $this->mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body='[
+            { "Business Task 0": { "level": 1, "estimated_duration": 7 } },
+            { "Business Task 1": { "level": 3, "estimated_duration": 4 } }
+        ]'));
+        $tasks = $this->service->getTasks();
+
+        $this->assertCount(2, $tasks);
+        $this->assertEquals(1 * 7, $tasks[0]->getWorkload());
+        $this->assertEquals('Business Task 0', $tasks[0]->getName());
+        $this->assertEquals(3 * 4, $tasks[1]->getWorkload());
+        $this->assertEquals('Business Task 1', $tasks[1]->getName());
     }
 }
--- tests/Service/ItTaskServiceTest.php
+++ tests/Service/ItTaskServiceTest.php
@@ -10,12 +10,37 @@ use PHPUnit\Framework\TestCase;
 
 class ItTaskServiceTest extends TestCase
 {
+    private $mockClient;
+
+    private $service;
+
+    protected function setUp()
+    {
+        $this->mockClient = $this->createMock(Client::class);
+        $this->service = new ItTaskService($this->mockClient, new ItTaskSerializer());
+    }
+
     public function testGetTasksWithEmptyList(): void
     {
-        $mockClient = $this->createMock(Client::class);
-        $mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body="[]"));
-        $service = new ItTaskService($mockClient, new ItTaskSerializer());
+        $this->mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body='[]'));
+        $tasks = $this->service->getTasks();
 
-        $this->assertEquals([], $service->getTasks());
+        $this->assertCount(0, $tasks);
+        $this->assertEquals([], $tasks);
+    }
+
+    public function testGetTasksWithValidList(): void
+    {
+        $this->mockClient->method('request')->willReturn(new Response($status=200, $headers=[], $body='[
+            { "zorluk": 3, "sure": 6, "id": "IT Task 0" },
+            { "zorluk": 4, "sure": 6, "id": "IT Task 1" }
+        ]'));
+        $tasks = $this->service->getTasks();
+
+        $this->assertCount(2, $tasks);
+        $this->assertEquals(3 * 6, $tasks[0]->getWorkload());
+        $this->assertEquals('IT Task 0', $tasks[0]->getName());
+        $this->assertEquals(4 * 6, $tasks[1]->getWorkload());
+        $this->assertEquals('IT Task 1', $tasks[1]->getName());
     }
 }
