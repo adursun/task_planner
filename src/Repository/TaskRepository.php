b2a79b3227715fd514208b58d046cf61eae2637c
Abdullah DURSUN <adursun@outlook.com.tr>
2020-04-12 20:00:56 +0300

    Add TaskRepository.resetAll method

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -19,32 +19,12 @@ class TaskRepository extends ServiceEntityRepository
         parent::__construct($registry, Task::class);
     }
 
-    // /**
-    //  * @return Task[] Returns an array of Task objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function resetAll()
     {
         return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
+            ->update()
+            ->set('t.remainingWorkload', 't.workload')
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Task
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
