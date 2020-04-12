56ad97d2305c0d1eeb3257764709b08f061b90b4
Abdullah DURSUN <adursun@outlook.com.tr>
2020-04-12 20:26:32 +0300

    Extract task assignment logic as TaskAssignerService

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Developer;
-use App\Entity\Task;
 use App\Repository\DeveloperRepository;
 use App\Repository\TaskRepository;
-use Doctrine\Common\Collections\Criteria;
+use App\Service\TaskAssignerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,68 +15,22 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param EntityManagerInterface $em
-     * @param DeveloperRepository $developerRepository
      * @param TaskRepository $taskRepository
+     * @param TaskAssignerService $taskAssigner
      * @return Response
      */
-    public function index(EntityManagerInterface $em, DeveloperRepository $developerRepository, TaskRepository $taskRepository)
+    public function index(TaskRepository $taskRepository, TaskAssignerService $taskAssigner)
     {
-        $taskRepository->resetAll();
+        $taskAssigner->assignTasks();
 
-        /** @var Developer[] $developers */
-        $developers = $developerRepository->findBy([], [
-            'hourlyWork' => 'DESC',
-        ]);
-
-        // trying new criteria api
-        $tasksCriteria = new Criteria();
-        $tasksCriteria->where(Criteria::expr()->neq('remainingWorkload', 0))->orderBy([
-            'workload' => 'DESC',
-        ]);
-        /** @var Task[] $tasks */
-        $tasks = $taskRepository->matching($tasksCriteria)->toArray();
-
-        $week = 0;
-
-        while (count($developers) > 0) {
-            $week++;
-
-            for ($i = 0; $i < 45; $i++) {
-                foreach ($developers as $developer) {
-                    if ($developer->getCurrentTask() === null) {
-                        $task = array_shift($tasks);
-
-                        if ($task === null) {
-                            $key = array_search($developer, $developers, true);
-                            unset($developers[$key]);
-                            continue;
-                        } else {
-                            $developer->setCurrentTask($task);
-                            $task->setDeveloper($developer);
-                            $task->setStartWeek($week);
-                        }
-                    }
-
-                    $developer->work($hours=1);
-
-                    if ($developer->getCurrentTask()->isFinished()) {
-                        $developer->getCurrentTask()->setEndWeek($week);
-                        $developer->setCurrentTask(null);
-                    }
-                }
-            }
-        }
-
-        $em->flush();
-
-        $resultingTasks = $taskRepository->findBy([], [
+        $tasks = $taskRepository->findBy([], [
             'workload' => 'DESC',
             'startWeek' => 'ASC',
             'endWeek' => 'ASC',
         ]);
 
         return $this->render('home/index.html.twig', [
-            'tasks' => $resultingTasks,
+            'tasks' => $tasks,
         ]);
     }
 }
---
+++ src/Service/TaskAssignerService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Developer;
+use App\Entity\Task;
+use App\Repository\DeveloperRepository;
+use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskAssignerService
+{
+    private EntityManagerInterface $em;
+
+    private TaskRepository $taskRepository;
+
+    private DeveloperRepository $developerRepository;
+
+    public function __construct(EntityManagerInterface $em, TaskRepository $taskRepository, DeveloperRepository $developerRepository)
+    {
+        $this->em = $em;
+        $this->taskRepository = $taskRepository;
+        $this->developerRepository = $developerRepository;
+    }
+
+    public function assignTasks(): void
+    {
+        $this->taskRepository->resetAll();
+
+        /** @var Developer[] $developers */
+        $developers = $this->developerRepository->findBy([], [
+            'hourlyWork' => 'DESC',
+        ]);
+
+        // trying new criteria api
+        $tasksCriteria = new Criteria();
+        $tasksCriteria->where(Criteria::expr()->neq('remainingWorkload', 0))->orderBy([
+            'workload' => 'DESC',
+        ]);
+        /** @var Task[] $tasks */
+        $tasks = $this->taskRepository->matching($tasksCriteria)->toArray();
+
+        $week = 0;
+
+        while (count($developers) > 0) {
+            $week++;
+
+            for ($i = 0; $i < 45; $i++) {
+                foreach ($developers as $developer) {
+                    if ($developer->getCurrentTask() === null) {
+                        $task = array_shift($tasks);
+
+                        if ($task === null) {
+                            $key = array_search($developer, $developers, true);
+                            unset($developers[$key]);
+                            continue;
+                        } else {
+                            $developer->setCurrentTask($task);
+                            $task->setDeveloper($developer);
+                            $task->setStartWeek($week);
+                        }
+                    }
+
+                    $developer->work($hours=1);
+
+                    if ($developer->getCurrentTask()->isFinished()) {
+                        $developer->getCurrentTask()->setEndWeek($week);
+                        $developer->setCurrentTask(null);
+                    }
+                }
+            }
+        }
+
+        $this->em->flush();
+    }
+}
