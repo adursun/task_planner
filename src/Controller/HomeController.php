8958d25c99c25c67c9d8e6fe88bc30e5f333c493
Abdullah DURSUN <adursun@outlook.com.tr>
2020-04-16 20:04:17 +0300

    Add missing PhpDoc

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home", methods={"GET", "POST"})
+     * @param Request $request
      * @param TaskRepository $taskRepository
      * @param TaskAssignerService $taskAssigner
      * @return Response
